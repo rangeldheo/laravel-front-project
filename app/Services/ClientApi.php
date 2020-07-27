f5bd325da984733ae50e47bfea5aa1801616b941
rangeldheo <rangeldheo@gmail.com>
2020-07-26 23:10:08 -0300

    Instalado Guzzle Http Client

--- app/Services/ClientApi.php
+++ app/Services/ClientApi.php
@@ -2,9 +2,34 @@
 
 namespace App\Services;
 
-use Illuminate\Database\Eloquent\Model;
 
-class ClientApi extends Model
+class ClientApi
 {
-    //
+    public static $httpClient;
+    public static $host;
+    public static $authorization;
+
+    public function __construct()
+    {
+
+    }
+
+    public static function get($endpoint)
+    {
+    }
+
+    public static function post($endpoint, $body)
+    {
+    }
+
+    public static function delete($endpoint)
+    {
+    }
+
+    public static function put($endpoint, $body)
+    {
+    }
+
+    public static function patch($endpoint, $body){        
+    }
 }
